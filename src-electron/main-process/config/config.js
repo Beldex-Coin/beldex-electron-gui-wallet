c9907036f6ec63eab1279f410debfca79a65d045
Munavver <munavver@beldex.io>
2026-08-17 11:30:42 +0530

    Feature to  freeze swapExchangeList in config

--- src-electron/main-process/config/config.js
+++ src-electron/main-process/config/config.js
@@ -1,4 +1,9 @@
-export const swapExchangeList = {
+/**
+ * Swap exchange feature flags.
+ * Set a value to false to disable that provider at runtime.
+ * @type {{ changelly: boolean, quickex: boolean }}
+ */
+export const swapExchangeList = Object.freeze({
   changelly: true,
   quickex: true
-};
+});
